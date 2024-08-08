b72e87477823675de0ecb631ad23cd44b4990d82
YGainey <yousef.gainey@gmail.com>
2024-08-08 17:50:49 +0100

    broken

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,  } from 'react';
+import React, { useState, useEffect, } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import * as postService from '../../services/postService'
@@ -23,38 +23,37 @@ const Feed = ({ posts }) => {
             console.error(error);
         }
     };
+        return (
+            <div id="scrollableFeed" className="scrollable-container">
+                <InfiniteScroll
+                    dataLength={allPosts.length}
+                    next={fetchMorePosts}
+                    hasMore={hasMore}
+                    loader={<h4>Loading...</h4>}
+                    scrollableTarget="scrollableFeed"
+                >
+                    <section>
+                        {allPosts.map((post) => (
+                            <Link key={post._id} to={`/posts/post/${post._id}`}>
+                                <div className='feedpost' key={post._id}>
+                                    <p>{post.author.username}: {post.title}</p>
+                                    <p>{post.body}</p>
+                                    <div className='feedpostimg'>
+                                        {post.images.slice(0, 3).map((image) => (
+                                            <React.Fragment key={image.img}>
+                                                <img src={image.img} alt={image.description} />
+                                                <small>{image.description}</small>
+                                            </React.Fragment>
+                                        ))}
+                                    </div>
+                                </div>
+                            </Link>
+                        ))}
+                    </section>
+                </InfiniteScroll>
+            </div>
+        )
 
-const Feed = (props) => {
-    return (
-        <div id="scrollableFeed" className="scrollable-container">
-            <InfiniteScroll
-                dataLength={allPosts.length}
-                next={fetchMorePosts}
-                hasMore={hasMore}
-                loader={<h4>Loading...</h4>}
-                scrollableTarget="scrollableFeed"
-            >
-                <section>
-                    {allPosts.map((post) => (
-                        <Link key={post._id} to={`/posts/post/${post._id}`}>
-                        <div className='feedpost' key={post._id}>
-                            <p>{post.author.username}: {post.title}</p>
-                            <p>{post.body}</p>
-                            <div className='feedpostimg'>
-                                {post.images.slice(0, 3).map((image) => (
-                                    <React.Fragment key={image.img}>
-                                        <img src={image.img} alt={image.description} />
-                                        <small>{image.description}</small>
-                                    </React.Fragment>
-                                ))}
-                            </div>
-                        </div>
-                        </Link>
-                    ))}
-                </section>
-            </InfiniteScroll>
-        </div>
-    );
-};
+}
 
 export default Feed;
